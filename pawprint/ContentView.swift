1689dd7e00b1912623a8009564379aeec0ff40b8
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-06-25 20:34:51 +0700

    Initial Commit

---
+++ pawprint/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pawprint
+//
+//  Created by Ivan Nur Ilham Syah on 25/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pawprint/pawprintApp.swift
@@ -0,0 +1,17 @@
+//
+//  pawprintApp.swift
+//  pawprint
+//
+//  Created by Ivan Nur Ilham Syah on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct pawprintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
