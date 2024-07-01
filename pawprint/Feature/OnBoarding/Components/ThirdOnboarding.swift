543aeeae575151fc8222d48bf394400f7fc33ead
charlot <hai.charlot@gmail.com>
2024-07-01 23:25:37 +0700

    feat: splash screen view (still without the animation) feat: onboarding view and components

---
+++ pawprint/Feature/OnBoarding/Components/ThirdOnboarding.swift
@@ -0,0 +1,17 @@
+//
+//  ThirdOnboarding.swift
+//  pawprint
+//
+//  Created by Maria Charlotta on 01/07/24.
+//
+
+import SwiftUI
+
+struct ThirdOnboarding: View {
+    var body: some View {
+        ZStack {
+            Image("onboard3")
+                .padding(.bottom, 100)
+        }
+    }
+}
