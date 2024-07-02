64c65e83e9dac3ae1cc46721edcde9f30c8bf459
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-07-02 14:37:25 +0700

    feat(handler): create camera handler

---
+++ pawprint/Handlers/CameraHandler.swift
@@ -0,0 +1,78 @@
+//
+//  CameraHandler.swift
+//  pawprint
+//
+//  Created by Ivan Nur Ilham Syah on 02/07/24.
+//
+
+import Foundation
+import AVFoundation
+
+class CameraHandler {
+    var session: AVCaptureSession?
+    var delegate: AVCapturePhotoCaptureDelegate?
+    
+    let output: AVCapturePhotoOutput = AVCapturePhotoOutput()
+    let preview: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer()
+    
+    func start(delegate: AVCapturePhotoCaptureDelegate, completion: @escaping (Error?) -> ()) {
+        self.delegate = delegate
+        
+    }
+    
+    private func checkPermissions(completion: @escaping (Error?) -> ()) {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+            
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: .video) { [weak self] granted in
+                guard granted else {
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    self?.setupCamera(completion: completion)
+                }
+            }
+        case .restricted:
+            break
+        case .denied:
+            break
+        case .authorized:
+            setupCamera(completion: completion)
+        @unknown default:
+            break
+        }
+    }
+    
+    private func setupCamera(completion: @escaping (Error?) -> ()) {
+        let session = AVCaptureSession()
+        
+        if let device = AVCaptureDevice.default(for: .video) {
+            do {
+                let input = try AVCaptureDeviceInput(device: device)
+                
+                if session.canAddInput(input) {
+                    session.addInput(input)
+                }
+                
+                if session.canAddOutput(output) {
+                    session.addOutput(output)
+                }
+                
+                preview.videoGravity = .resizeAspectFill
+                preview.session = session
+                
+                DispatchQueue.global(qos: .userInitiated).async {
+                    session.startRunning()
+                }
+                self.session = session
+            } catch {
+                completion(error)
+            }
+        }
+    }
+    
+    func capturePhoto(with settings: AVCapturePhotoSettings = AVCapturePhotoSettings()) {
+        output.capturePhoto(with: settings, delegate: self.delegate!)
+    }
+}
