b5d8a56b5c909c423c98e5f0461caf64347a58bb
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-07-03 14:59:05 +0700

    fix: revamp handwriting analyze result view

--- pawprint/Views/HandwritingAssessmentResultView.swift
+++ pawprint/Views/HandwritingAssessmentResultView.swift
@@ -10,21 +10,58 @@ import SwiftUI
 struct HandwritingAssessmentResultView: View {
     var body: some View {
         NavigationStack {
-            VStack(alignment: .leading) {
-                Text("How Paw see your handwriting")
-                    .font(.system(size: 48).bold())
-                ZStack(alignment: .leading) {
-                    VStack(spacing: 16) {
-                        Text("Readability")
-                            .font(.system(size: 40))
-                        Text("80%")
-                            .font(.system(size: 96))
-                            .fontWeight(.heavy)
-                        Text("effervessent elves swiftly sew")
-                            .font(.system(size: 60))
-                            .fontWeight(.medium)
+            ZStack{
+                Image(.doodleBackground)
+                
+                VStack(alignment: .leading) {
+                    Text("How Paw see your handwriting")
+                        .font(.system(size: 36))
+                        .fontWeight(.semibold)
+                    ZStack(alignment: .leading) {
+                        VStack(spacing: 32) {
+                            VStack(spacing: 48){
+                                Text("Readability")
+                                    .font(.system(size: 40))
+                                Text("80%")
+                                    .font(.system(size: 96))
+                                    .fontWeight(.heavy)
+                                    .overlay {
+                                        Image(.result)
+                                            .offset(x: 24)
+                                    }
+                            }
+                            
+                            Text("Readability Letters Detail (e,f,s) On iPad")
+                                .font(.system(size: 24))
+                                .fontWeight(.medium)
+                            
+                            Text("e: 4/5 readable,  f: 4/4 readable,  s: 7/8 readable")
+                                .font(.system(size: 40))
+                                .fontWeight(.medium)
+                            
+                            HStack{
+                                Image(.handwritingExample)
+                                    .padding()
+                                    .overlay {
+                                        RoundedRectangle(cornerRadius: 20)
+                                            .stroke(
+                                                .red,
+                                                lineWidth: 4
+                                            )
+                                    }
+                                Spacer()
+                                Image(systemName: "arrow.right")
+                                    .font(.system(size: 60))
+                                    .fontWeight(.medium)
+                                Spacer()
+                                Text("effervessent elves swiftly sew")
+                                    .font(.system(size: 60))
+                                    .fontWeight(.medium)
+                                    .multilineTextAlignment(.center)
+                            }
                             .padding(.horizontal, 32)
                             .padding(.vertical, 24)
+                            .frame(maxWidth: .infinity)
                             .overlay {
                                 RoundedRectangle(cornerRadius: 20)
                                     .stroke(
@@ -34,46 +71,43 @@ struct HandwritingAssessmentResultView: View {
                                         )
                                     )
                             }
-                        Text("Others Letter need to improve is c, m, n.")
-                            .font(.system(size: 32))
-                            .fontWeight(.medium)
-                    }
-                    .padding(.vertical, 72)
-                    .frame(maxWidth: .infinity)
-                    .border(.black, width: 16)
-                    
-                    Image(.catTeaching)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(maxWidth: 256)
-                        .offset(x: -20, y: 232)
-                }
-                
-                Spacer()
-                
-                HStack {
-                    Button(action: {}) {
-                        HStack {
-                            Text("Repeat")
-                            Image(systemName: "repeat")
+                            
                         }
+                        .padding(.vertical, 72)
+                        .frame(maxWidth: .infinity)
+                        
+                        //                    Image(.catTeaching)
+                        //                        .resizable()
+                        //                        .scaledToFit()
+                        //                        .frame(maxWidth: 256)
+                        //                        .offset(x: -20, y: 232)
                     }
-                    .buttonStyle(PawPrintButtonStyle())
                     
                     Spacer()
                     
-                    Button(action: {}) {
-                        HStack {
-                            Text("Finish")
-                            Image(systemName: "flag.2.crossed")
+                    HStack {
+    //                    Button(action: {}) {
+    //                        HStack {
+    //                            Text("Repeat")
+    //                            Image(systemName: "repeat")
+    //                        }
+    //                    }
+    //                    .buttonStyle(PawPrintButtonStyle())
+                        
+                        Spacer()
+                        
+                        Button(action: {}) {
+                            HStack {
+                                Text("Finish")
+                                Image(systemName: "flag.2.crossed")
+                            }
                         }
+                        .buttonStyle(PawPrintButtonStyle())
                     }
-                    .buttonStyle(PawPrintButtonStyle())
                 }
+                .padding(.horizontal, 116)
+                .padding(.vertical, 64)
             }
-            .padding(.horizontal, 116)
-            .padding(.top, 116)
-            .padding(.bottom, 16)
             .navigationBarBackButtonHidden()
         }
     }
